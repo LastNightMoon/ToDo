144c39318c88a06ce8cbdfadb25cda30b4c88289
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2025-02-18 17:32:03 +0300

    bug logger fix

--- back/utils/logger.py
+++ back/utils/logger.py
@@ -1,17 +1,23 @@
 import datetime
 class Logger:
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
     @staticmethod
-    def log(messsage, level):
-        print(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), level + ':', messsage)
+    def log(message: str, level):
+        print(f'{datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")} -: {level} {message}')
         
     @staticmethod
     def log_func_decorator(func):
         def wrapper(*args, **kwargs):
-            Logger.log(f'Вызван метод: {func}', 'INFO')
+            Logger.log(f'Вызван метод: {func}', Logger.INFO)
             try:
                 res = func(*args, **kwargs)
-                Logger.log(f'закончен метод: {func}', 'INFO')
+                Logger.log(f'закончен метод: {func}', Logger.INFO)
                 return res
             except Exception as e:
-                Logger.log(f'Получена ошибка: {e}', 'ERROR')
+                Logger.log(f'Получена ошибка: {e}', Logger.ERROR)
                 return {'code': 500, 'msg': e}
+        return wrapper
