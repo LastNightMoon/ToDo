1a5c8786b67bcf195e1705b0a41c46620a4a3c8d
EdaxAnimae <romses.2005@gmail.com>
2025-02-23 18:01:54 +0300

    endpoint login finished

--- back/main.py
+++ back/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from utils.variable_environment import VarEnv
 from DataBaseManager.models import Users, Groups, Tasks
-from models import UserAuth
+from models import UserAuth, UserLogin
 import sqlalchemy
 from sqlalchemy.dialects.postgresql import insert
 from DataBaseManager import db
@@ -23,6 +23,19 @@ async def registerUser(item: UserAuth):
         result = True
         msg = "Ok"
         db.execute_commit(sqlalchemy.insert(Users).values(login=item.login, password=item.password))
+    return JSONResponse(content={"result": result, "msg": msg}, status_code=200)
+
+@app.post("/login", response_class=JSONResponse)
+async def authenticate(item: UserLogin):
+    if not item.login or not item.password:
+        result = False
+        msg = "Login and password fields cannot be empty"
+    elif not db.select(sqlalchemy.select(Users).where(Users.login==item.login, Users.password==item.password)):
+        result = False
+        msg = "The login or password entered is incorrect"
+    else:
+        result = True
+        msg = "Ok"
     return JSONResponse(content={"result": result, "msg": msg}, status_code=200)
 
 if __name__ == "__main__":
